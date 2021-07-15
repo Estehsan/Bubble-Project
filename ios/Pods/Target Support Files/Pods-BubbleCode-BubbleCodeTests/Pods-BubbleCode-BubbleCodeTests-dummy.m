c188b82aa44a93f37712327e4d6fad056f45ec35
Estehsan <estehsaan@gmail.com>
2021-07-15 15:13:17 +0500

    remove gitignore files

--- ios/Pods/Target Support Files/Pods-BubbleCode-BubbleCodeTests/Pods-BubbleCode-BubbleCodeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BubbleCode_BubbleCodeTests : NSObject
-@end
-@implementation PodsDummy_Pods_BubbleCode_BubbleCodeTests
-@end
--- ios/Pods/Target Support Files/Pods-BubbleCode/Pods-BubbleCode-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BubbleCode : NSObject
-@end
-@implementation PodsDummy_Pods_BubbleCode
-@end
