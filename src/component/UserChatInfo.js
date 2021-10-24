6dbe3f58e8c62fcda3b81759b5683dbaed6426f0
aliahmed1 <saliahmed11111@gmail.com>
2021-10-24 16:22:23 +0500

    invite notification

--- src/component/UserChatInfo.js
+++ src/component/UserChatInfo.js
@@ -18,6 +18,8 @@ import { useNavigation } from "@react-navigation/native";
 import { block } from "react-native-reanimated";
 import { firestore } from "../db/firebase";
 import firebase from "firebase/app";
+import OneSignal from "react-native-onesignal";
+
 
 const UserChatInfo = ({
   currentUserData,
@@ -32,15 +34,19 @@ const UserChatInfo = ({
   const [candy, setCandy] = useState(0)
   const [commonTeams, setCommonTeams] = useState([]);
   const [notification, setNotification] = useState([]);
+  const [notificationId, setNotificationId] = useState("")
   const navigation = useNavigation();
 
 
   useEffect(() => {
     firestore.collection("users").doc(currentUserData.id).onSnapshot((doc) => {
-      setNotification(doc.data().notification)
+      // setNotification(doc.data().notification)
       setCandy(doc.data().candy)
     })
-    console.log(notification)
+    firestore.collection("users").doc(id).onSnapshot((doc) => {
+      setNotificationId(doc.data().notificationId)
+    })
+    // console.log(notification)
   }, [])
 
 
@@ -79,21 +85,58 @@ const UserChatInfo = ({
 
     if (candy > 0) {
       await firestore.collection("users").doc(currentUserData.id).update({
-        notification: firebase.firestore.FieldValue.arrayUnion(id),
+        // notification: firebase.firestore.FieldValue.arrayUnion(id),
         candy: firebase.firestore.FieldValue.increment(-1)
       })
 
-      await firestore.collection("users").doc(id).update({
-        notification: firebase.firestore.FieldValue.arrayUnion(currentUserData.id)
-      })
+      let externalUserId = notificationId; // You will supply the external user id to the OneSignal SDK
+
+      // Setting External User Id with Callback Available in SDK Version 3.9.3+
+      OneSignal.setExternalUserId(externalUserId, (results) => {
+        // The results will contain push and email success statuses
+        console.log("Results of setting external user id");
+        console.log(results);
+
+        // Push can be expected in almost every situation with a success status, but
+        // as a pre-caution its good to verify it exists
+        if (results.push && results.push.success) {
+          console.log("Results of setting external user id push status:");
+          console.log(results.push.success);
+        }
+      });
+
+      const notification = {
+        contents: {
+          en: `Dear ${name}, ${currentUserData.userName} has invited you to have a Chat`,
+        },
+        include_player_ids: [externalUserId],
+      };
+
+      const jsonStri = JSON.stringify(notification);
+
+      await OneSignal.postNotification(
+        jsonStri,
+        (success) => {
+          console.log("Success:", success);
+        },
+        (error) => {
+          console.log("Error:", error);
+        }
+      );
+
+
+
       setNotificationModel(false)
     }
 
-    else{
+    else {
       Alert.alert("You Don't have enough candy")
       setNotificationModel(false)
 
     }
+
+
+
   }
 
   return (
@@ -196,20 +239,21 @@ const UserChatInfo = ({
           <Text>{gender}</Text>
         </View>
         <View style={styles.rContainer}>
-          {notification && notification.includes(id) ? <View style={{flex: 1}}></View>
-            :
-            <TouchableOpacity
-              style={styles.ListOfUsers}
-              onPress={() => {
-                setNotificationModel(true)
-              }}>
-              <Image
-                style={{ height: 50, width: 50, borderRadius: 50 }}
-                resizeMode="contain"
-                source={require("./../assets/images/rose.png")}
-              />
-            </TouchableOpacity>
-          }
+          {/* {
+          notification && notification.includes(id) ? <View style={{ flex: 1 }}></View>
+            : */}
+          <TouchableOpacity
+            style={styles.ListOfUsers}
+            onPress={() => {
+              setNotificationModel(true)
+            }}>
+            <Image
+              style={{ height: 50, width: 50, borderRadius: 50 }}
+              resizeMode="contain"
+              source={require("./../assets/images/rose.png")}
+            />
+          </TouchableOpacity>
+          {/* } */}
           <TouchableOpacity onPress={() => {
             navigation.navigate("ChatUser", {
               currentUserId: currentUserData.id,
